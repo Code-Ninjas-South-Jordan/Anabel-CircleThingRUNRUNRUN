3b758ed0c68ae1bda12cd07526bb20ebfdd02798
Code Ninjas 4 <codeninjassouthjordan@gmail.com>
2021-04-30 12:06:11 -0600

    jhgfd

---
+++ Anabel-PolyRun/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Default Jumping Power")]
+    public float jumpPower = 6f;
+    [Header("Boolean onGround")]
+    public bool onGround = false;
+    float posX = 0.0f;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        posX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space) && onGround)
+        {
+            rb.AddForce(Vector3.up * (jumpPower * rb.mass * rb.gravityScale * 20.0f));
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            onGround = false;
+        }
+    } 
+}
